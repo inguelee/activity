617cae7445b6a118a030d69bd1e9ce2e239bf35c
inguelee <55696282+inguelee@users.noreply.github.com>
2022-10-06 19:04:03 +0900

    2445.py

---
+++ 백준알고/별똥기/8.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+x = 0
+
+for i in range(1, 2 * N):
+    if i > N:
+        x -= 1
+    else:
+        x += 1
+    s = '*' * x
+    print(s.ljust(N), end='')
+    print(s.rjust(N))
