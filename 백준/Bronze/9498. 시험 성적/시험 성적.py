6c074914df063005a4a1de7e958138592ea8729f
inguelee <55696282+inguelee@users.noreply.github.com>
2022-11-02 03:08:00 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+if 100 >= N and N >= 90:
+    print('A')
+elif 90 > N and N >= 80:
+    print('B')
+elif 80 > N and N >= 70:
+    print('C')
+elif 70 > N and N >= 60:
+    print('D')
+else:
+    print('F')
