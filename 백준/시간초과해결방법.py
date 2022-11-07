2f8d9285819954c2cf705ab99af344036c8741bc
inguelee <55696282+inguelee@users.noreply.github.com>
2022-11-07 21:58:17 +0900

    Create 시간초과해결방법.py

---
+++ 백준/시간초과해결방법.py
@@ -0,0 +1,44 @@
+# 1. input()보다 sys.stdin.readline()이 더 빠르다.
+import sys
+
+# 한 개의 정수를 입력
+n = int(sys.stdin.readline())
+
+# 한 개의 문자를 입력
+n = sys.stdin.readline()
+
+
+# 띄어쓰기로 구분된 두 개의 정수를 입력
+a, b = [int(x) for x in sys.stdin.readline().split()]
+
+# 띄어쓰기로 두 문자열 입력
+c, d = sys.stdin.readline().split()
+
+# 띄어쓰기로 구분되어 있는 배열을 입력
+arr = [int(x) for x in sys.stdin.readline().split()]
+
+# 2. if문에 두 개 이상의 조건이 주어질 때, 조건의 순서를 생각하면서 작성한다.
+if condition1 and condition2:
+  pass
+
+if condition1 or condition2:
+  pass
+
+# 3. 문자열을 붙일 때 +연산 대신 join()을 사용한다.
+## + 연산
+for str in string_list:
+	concatenate_string += str
+
+## join()함수
+concatenate_string = "".join(string_list)
+
+# 4. List Comprehension을 사용한다.
+## for loop
+numbers = []
+for i in range(1000):
+	numbers.append(i) # (X)
+    
+## comprehension
+numbers = [i for i in range(1000)]
+
+# + 배열을 만들때 for loop을 돌리는 것보다 multiplication으로 할당하는 것이 빠르다!
